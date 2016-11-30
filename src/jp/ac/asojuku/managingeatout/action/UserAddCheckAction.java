19f2114f15a752799bcb4e76b250fcbe41dd6147
aso1401151 <1401151@st.asojuku.ac.jp>
2016-11-30 10:40:28 +0900

    Add files via upload

---
+++ src/jp/ac/asojuku/managingeatout/action/UserAddCheckAction.java
@@ -0,0 +1,69 @@
+package jp.ac.asojuku.food.action;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jp.ac.asojuku.food.check.UserAddCheck;
+import jp.ac.asojuku.food.dto.UserCheckStock;
+
+public class UserAddCheckAction extends HttpServlet{
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		UserCheckStock UCS = new UserCheckStock();
+		UserAddCheck UAC = new UserAddCheck();
+
+		UCS.setMail(req.getParameter("mail"));
+		UCS.setRe_mail(req.getParameter("re_mail"));
+		UCS.setUseid(req.getParameter("use_ID"));
+		UCS.setPass(req.getParameter("passward"));
+		UCS.setRe_pass(req.getParameter("re_passward"));
+		UCS.setName_sei(req.getParameter("name_sei"));
+		UCS.setName_mei(req.getParameter("name_mei"));
+		UCS.setFuri_sei(req.getParameter("furi_sei"));
+		UCS.setFuri_mei(req.getParameter("furi_mei"));
+		UCS.setPref(req.getParameter("pref"));
+		UCS.setAddr(req.getParameter("address"));
+		UCS.setBirth_y(req.getParameter("birth_y"));
+		UCS.setBirth_m(req.getParameter("birth_m"));
+		UCS.setBirth_d(req.getParameter("birth_d"));
+		UCS.setSex(req.getParameter("sex"));
+
+		List<String> list = UAC.user(UCS);
+
+		if(list.size() == 0){
+			req.setAttribute("mail",req.getParameter("mail") );
+			req.setAttribute("pass",req.getParameter("passward") );
+			req.setAttribute("userid",req.getParameter("use_ID") );
+			req.setAttribute("names",req.getParameter("name_sei") );
+			req.setAttribute("namem",req.getParameter("name_mei") );
+			req.setAttribute("furis",req.getParameter("furi_sei") );
+			req.setAttribute("furim",req.getParameter("furi_mei") );
+			req.setAttribute("pref",req.getParameter("pref") );
+			req.setAttribute("addr",req.getParameter("address") );
+			req.setAttribute("birthy",req.getParameter("birth_y") );
+			req.setAttribute("birthm",req.getParameter("birth_m") );
+			req.setAttribute("birthd",req.getParameter("birth_d") );
+			req.setAttribute("sex",req.getParameter("sex") );
+
+			RequestDispatcher rd = req.getRequestDispatcher("view/user_check.jsp");
+			rd.forward(req, resp);
+			return;
+		}else{
+			req.setAttribute("error_msg","入力が正しくありません。再度入力してください" );
+
+			req.setAttribute("LIST", list);
+
+			RequestDispatcher rd = req.getRequestDispatcher("view/user_add.jsp");
+			rd.forward(req, resp);
+			return;
+		}
+
+
+	}
+}
---
+++ src/jp/ac/asojuku/managingeatout/action/UserAddComp.java
@@ -0,0 +1,62 @@
+package jp.ac.asojuku.food.action;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.naming.NamingException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import jp.ac.asojuku.food.bo.UserAddBo;
+
+public class UserAddComp extends HttpServlet {
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session = req.getSession(false);
+
+		UserAddBo UAB = new UserAddBo();
+
+		String us_mail = (String)session.getAttribute("mail");
+		String us_pass = (String)session.getAttribute("pass");
+		String us_id = (String)session.getAttribute("userid");
+		String names = (String)session.getAttribute("names");
+		String namem = (String)session.getAttribute("namem");
+		String perf = (String)session.getAttribute("pref");
+		String addr = (String)session.getAttribute("addr");
+		String birthy = (String)session.getAttribute("birthy");
+		String birthm = (String)session.getAttribute("birthm");
+		String birthd = (String)session.getAttribute("birthd");
+		String us_gender = (String)session.getAttribute("sex");
+
+		String us_name = names + namem;
+		String us_address = perf + addr;
+		String us_birth = birthy + "-"+  birthm + "-" + birthd;
+
+		try {
+			UAB.insertMemberInfo(us_id,us_name,us_birth,us_gender,us_mail,us_address,us_pass);
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+			System.out.println("登録NGs");
+			RequestDispatcher rd = req.getRequestDispatcher("view/user_add.jsp");
+			rd.forward(req, resp);
+			return;
+		} catch (NamingException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+			System.out.println("登録NGn");
+			RequestDispatcher rd = req.getRequestDispatcher("view/user_add.jsp");
+			rd.forward(req, resp);
+			return;
+		}
+
+		RequestDispatcher rd = req.getRequestDispatcher("view/user_comp.jsp");
+		rd.forward(req, resp);
+	}
+}
+
+

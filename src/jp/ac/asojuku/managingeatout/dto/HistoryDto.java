70a42311d1bdabcf3a3354f9bb660774cd0381f8
aso1401151 <1401151@st.asojuku.ac.jp>
2016-11-30 10:41:28 +0900

    Add files via upload

---
+++ src/jp/ac/asojuku/managingeatout/dto/HistoryDto.java
@@ -0,0 +1,51 @@
+package jp.ac.asojuku.food.dto;
+
+public class HistoryDto {
+	
+	private String hs_add;
+	private String re_name;
+	private String re_place;
+	/**
+	 * hs_addを取得します。
+	 * @return hs_add
+	 */
+	public String getHs_add() {
+	    return hs_add;
+	}
+	/**
+	 * hs_addを設定します。
+	 * @param hs_add hs_add
+	 */
+	public void setHs_add(String hs_add) {
+	    this.hs_add = hs_add;
+	}
+	/**
+	 * re_nameを取得します。
+	 * @return re_name
+	 */
+	public String getRe_name() {
+	    return re_name;
+	}
+	/**
+	 * re_nameを設定します。
+	 * @param re_name re_name
+	 */
+	public void setRe_name(String re_name) {
+	    this.re_name = re_name;
+	}
+	/**
+	 * re_placeを取得します。
+	 * @return re_place
+	 */
+	public String getRe_place() {
+	    return re_place;
+	}
+	/**
+	 * re_placeを設定します。
+	 * @param re_place re_place
+	 */
+	public void setRe_place(String re_place) {
+	    this.re_place = re_place;
+	}
+
+}
---
+++ src/jp/ac/asojuku/managingeatout/dto/UserCheckStock.java
@@ -0,0 +1,199 @@
+package jp.ac.asojuku.food.dto;
+
+public class UserCheckStock {
+	private String mail;
+	/**
+	 * @return mail
+	 */
+	public String getMail() {
+		return mail;
+	}
+	/**
+	 * @param mail セットする mail
+	 */
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	/**
+	 * @return re_mail
+	 */
+	public String getRe_mail() {
+		return re_mail;
+	}
+	/**
+	 * @param re_mail セットする re_mail
+	 */
+	public void setRe_mail(String re_mail) {
+		this.re_mail = re_mail;
+	}
+	/**
+	 * @return useid
+	 */
+	public String getUseid() {
+		return useid;
+	}
+	/**
+	 * @param useid セットする useid
+	 */
+	public void setUseid(String useid) {
+		this.useid = useid;
+	}
+	/**
+	 * @return pass
+	 */
+	public String getPass() {
+		return pass;
+	}
+	/**
+	 * @param pass セットする pass
+	 */
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	/**
+	 * @return re_pass
+	 */
+	public String getRe_pass() {
+		return re_pass;
+	}
+	/**
+	 * @param re_pass セットする re_pass
+	 */
+	public void setRe_pass(String re_pass) {
+		this.re_pass = re_pass;
+	}
+	/**
+	 * @return name_sei
+	 */
+	public String getName_sei() {
+		return name_sei;
+	}
+	/**
+	 * @param name_sei セットする name_sei
+	 */
+	public void setName_sei(String name_sei) {
+		this.name_sei = name_sei;
+	}
+	/**
+	 * @return name_mei
+	 */
+	public String getName_mei() {
+		return name_mei;
+	}
+	/**
+	 * @param name_mei セットする name_mei
+	 */
+	public void setName_mei(String name_mei) {
+		this.name_mei = name_mei;
+	}
+	/**
+	 * @return furi_sei
+	 */
+	public String getFuri_sei() {
+		return furi_sei;
+	}
+	/**
+	 * @param furi_sei セットする furi_sei
+	 */
+	public void setFuri_sei(String furi_sei) {
+		this.furi_sei = furi_sei;
+	}
+	/**
+	 * @return furi_mei
+	 */
+	public String getFuri_mei() {
+		return furi_mei;
+	}
+	/**
+	 * @param furi_mei セットする furi_mei
+	 */
+	public void setFuri_mei(String furi_mei) {
+		this.furi_mei = furi_mei;
+	}
+	/**
+	 * @return pref
+	 */
+	public String getPref() {
+		return pref;
+	}
+	/**
+	 * @param pref セットする pref
+	 */
+	public void setPref(String pref) {
+		this.pref = pref;
+	}
+	/**
+	 * @return addr
+	 */
+	public String getAddr() {
+		return addr;
+	}
+	/**
+	 * @param addr セットする addr
+	 */
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	/**
+	 * @return birth_y
+	 */
+	public String getBirth_y() {
+		return birth_y;
+	}
+	/**
+	 * @param birth_y セットする birth_y
+	 */
+	public void setBirth_y(String birth_y) {
+		this.birth_y = birth_y;
+	}
+	/**
+	 * @return birth_m
+	 */
+	public String getBirth_m() {
+		return birth_m;
+	}
+	/**
+	 * @param birth_m セットする birth_m
+	 */
+	public void setBirth_m(String birth_m) {
+		this.birth_m = birth_m;
+	}
+	/**
+	 * @return birth_d
+	 */
+	public String getBirth_d() {
+		return birth_d;
+	}
+	/**
+	 * @param birth_d セットする birth_d
+	 */
+	public void setBirth_d(String birth_d) {
+		this.birth_d = birth_d;
+	}
+	/**
+	 * @return sex
+	 */
+	public String getSex() {
+		return sex;
+	}
+	/**
+	 * @param sex セットする sex
+	 */
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	private String re_mail;
+	private String useid;
+	private String pass;
+	private String re_pass;
+	private String name_sei;
+	private String name_mei;
+	private String furi_sei;
+	private String furi_mei;
+	private String pref;
+	private String addr;
+	private String birth_y;
+	private String birth_m;
+	private String birth_d;
+	private String sex;
+}
